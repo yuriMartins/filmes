4e6e1eb6a475a02692ba0321bde62d51619aec70
yuriMartins <ysm281@gmail.com>
2020-09-10 11:54:14 -0300

    Catálago de Filmes
    Aplicação Web que demostra filmes disponíveis

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import './style.css'
+import Routes from './routes';
+
+class App extends Component{
+  render(){
+    return (
+      <div className= " app" >
+          <Routes/>
+      </div>
+    );
+
+  }
+}
+
+
+export default App;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
+import './home.css';
+
+class Home extends Component{
+
+    constructor(props){
+    super(props);
+    this.state = {
+        filmes: []
+    };
+
+    this.loadFilmes = this.loadFilmes.bind(this);
+}
+
+componentDidMount(){
+    this.loadFilmes();
+}
+
+loadFilmes(){
+    let url = 'https://sujeitoprogramador.com/r-api/?api=filmes/'
+    fetch(url)
+    .then((r) => r.json())
+    .then((json) => {
+        this.setState({filmes: json});
+        console.log(json);
+    });
+}
+
+
+    render(){
+        return(
+            <div className="container">
+                <div className="lista-filmes">
+                    {this.state.filmes.map((filme) => {
+                        return(
+                            <article key={filme.id} className="filme">
+                                <strong> {filme.nome} </strong>
+                                <img src={filme.foto} alt="Capa" />
+                                <Link to={`/filme/${filme.id}`} >Acessar</Link>
+                            </article>
+                        )
+                    })}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Home;
